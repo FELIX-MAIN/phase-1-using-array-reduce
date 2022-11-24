fb3003521d1a24571387e16ce4af54a3ae7a1688
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-24 22:38:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Code your solution here
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const initialValue = 0;
+const totalBatteries = batteryBatches.reduce(
+    (accumulator, currentvalue) => accumulator + currentvalue,
+    initialValue
+);
+
+console.log(totalBatteries);
